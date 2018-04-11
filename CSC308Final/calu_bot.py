128be6ef4cc8c205be5e7c79c2a9ef952d3fe521
die4700 <die4700@calu.edu>
2018-04-11 13:06:00 -0400

    Minor changes to calu_bot, added comments

--- CSC308Final/calu_bot.py
+++ CSC308Final/calu_bot.py
@@ -11,17 +11,23 @@ import config
 # Import login credentials
 import credentials
 
+# Function to log bot into reddit
 def login():
     obj = praw.Reddit(username = credentials.username,
                 password = credentials.password,
                 client_id = credentials.client_id,
-                client_secret = credentials.client_secret)
+                client_secret = credentials.client_secret,
+                user_agent = "CalU Python bot test v0.1")
     return obj
 
+# Function that searches for string within comments on /r/test
 def bot(obj):
     for comment in obj.subreddit('test').comments(limit=25):
-        if "python" in comment.body:
+        if "test" in comment.body:
             print("String found!")
 
+# Creates login object
 obj = login()
+
+# Runs bot using login object
 bot(obj)
